8c895c0fdbbc9388bc07b637519528c358b24a89
zhangjian <zhangjian@shanghaitech.edu.cn>
2022-04-10 17:39:34 +0800

    minor fix

--- libfs/fusionfsio.c
+++ libfs/fusionfsio.c
@@ -367,6 +367,7 @@ static uinode *get_inode(const char *fname, ufile *fp) {
         if (!target) {
                 target = (uinode *)malloc(sizeof(uinode));
                 memset(target, 0, sizeof(uinode));
+                strcpy(target->fname, fname);
                 uinode_table_insert(target);
         }
 
@@ -377,7 +378,6 @@ static uinode *get_inode(const char *fname, ufile *fp) {
                         pthread_spin_init(&target->sq_tree_lock, 0);
                 }
 #endif
-                if (target->ref == 0) strcpy(target->fname, fname);
 
                 fp->inode = target;
                 fp->inode_idx = target->ref;
